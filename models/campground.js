bae2d561a26cb33106ea54e1d73880f479a23330
charanReddy-bit <rsaicharan06@gmail.com>
2021-06-22 17:24:22 +0530

    fourth commit

--- models/campground.js
+++ models/campground.js
@@ -1,24 +1,37 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
 
-const Schema=mongoose.Schema;
-const CampgroundSchema= new Schema({
+
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const CampgroundSchema = new Schema({
     title: String,
-    image:String,
-    price : Number,
-    description : String,
-    location : String ,
+    images: [ImageSchema],
+    price: Number,
+    description: String,
+    location: String,
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    },  
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
     ]
-
 });
+
 
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
@@ -31,4 +44,4 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
     }
 })
 
-module.exports=  mongoose.model('Campground',CampgroundSchema );
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -23,14 +23,23 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() *20) +10;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author : '60b44a0a9ae19a0328e0005d',
+            author: '60b44a0a9ae19a0328e0005d',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:'https://source.unsplash.com/collection/483251',
-            description:'  Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta a, voluptate ipsa porro esse optio. Quos optio illo provident minima. Est, assumenda! Possimus non optio neque deleniti incidunt laboriosam maxime?',
-            price
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dizwvjzrj/image/upload/v1624338914/YelpCamp/tho6tzrdkbzdvvxqeuzc.jpg',
+                    filename: 'YelpCamp/tho6tzrdkbzdvvxqeuzc'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dizwvjzrj/image/upload/v1624338912/YelpCamp/cbjswaznofnpaskpsefd.jpg',
+                    filename: 'YelpCamp/cbjswaznofnpaskpsefd'
+                }
+            ]
         })
         await camp.save();
     }
